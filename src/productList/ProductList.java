e69b5e7587269162995c8053c316fa3e58f9a4b4
Timerkhan <timerkhan-94@mail.ru>
2015-04-01 17:27:46 +0300

    chars

--- src/productList/ProductList.java
+++ src/productList/ProductList.java
@@ -8,4 +8,21 @@ public class ProductList {
             return "Enter the Name";
         }
     }
+
+    public static String getNumberStandartChars(String a) {
+        a.toLowerCase();
+        char[] c_arr = a.toCharArray();
+        for (int i = 0; i < c_arr.length; i++) {
+            System.out.println(c_arr[i]);
+        }
+        String b = new String(c_arr);
+        return b;
+    }
+
+
+    public static void main(String[] args) {
+        String n = "heLlo";
+        System.out.println(getNumberStandartChars(n));
+    }
 }
+
