012c976ede52d8a691e4a01696de047239e36696
Timerkhan <timerkhan-94@mail.ru>
2015-04-02 15:02:59 +0300

    fast chars work and speed

--- src/productList/ProductList.java
+++ src/productList/ProductList.java
@@ -13,12 +13,13 @@ public class ProductList {
         char[] charList = a.toCharArray();
         StringBuilder k = new StringBuilder("");
         for (int i = 0; i < charList.length; i++) {
+            charList[i] = Character.toLowerCase(charList[i]);
             if (charList[i] >= '\u0030' && charList[i] <= '\u0039' || charList[i] >= '\u0061' && charList[i] <= '\u007A'
                     || charList[i] >= '\u0410' && charList[i] <= '\u044F' || charList[i] >= '\u0041' && charList[i] <= '\u005A') {
                 k.append(charList[i]);
             }
         }
-        return k.toString().toLowerCase();
+        return k.toString();
     }
 
 
