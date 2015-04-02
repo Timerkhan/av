25237ebbf238bb9ffd74471a406f35efe0ef9245
Timerkhan <timerkhan-94@mail.ru>
2015-04-02 16:00:02 +0300

    fast chars work and speed

--- src/productList/ProductList.java
+++ src/productList/ProductList.java
@@ -10,19 +10,24 @@ public class ProductList {
     }
 
     public static String getNumberStandartChars(String a) {
-        char[] charList = a.toCharArray();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < charList.length; i++) {
-            if (charList[i] >= '\u0030' && charList[i] <= '\u0039' || charList[i] >= '\u0061' && charList[i] <= '\u007A'
-                    || charList[i] >= '\u0430' && charList[i] <= '\u044F' ) {
-                stringBuilder.append(charList[i]);
+        try {
+            char[] charList = a.toCharArray();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < charList.length; i++) {
+                if (charList[i] >= '\u0030' && charList[i] <= '\u0039' || charList[i] >= '\u0061' && charList[i] <= '\u007A'
+                        || charList[i] >= '\u0430' && charList[i] <= '\u044F') {
+                    stringBuilder.append(charList[i]);
+                }
+                if (charList[i] >= '\u0041' && charList[i] <= '\u005A' || charList[i] >= '\u0410' && charList[i] <= '\u042F') {
+                    stringBuilder.append(Character.toLowerCase(charList[i]));
+                }
             }
-            if(charList[i] >= '\u0041' && charList[i] <= '\u005A' || charList[i] >= '\u0410' && charList[i] <= '\u042F'){
-                stringBuilder.append(Character.toLowerCase(charList[i]));
-            }
+            return stringBuilder.toString();
+        } catch (NullPointerException e) {
+            return "Enter the Name";
         }
-        return stringBuilder.toString();
     }
+
 
 
     public static void main(String[] args) {
